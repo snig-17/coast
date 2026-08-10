f85103ddb35659da05d925125571791feed59736
Snigdha Tiwari <snigdha.17.tiwari@gmail.com>
2026-08-10 15:04:43 +0100

    ci: typecheck + engine/core tests on push and PR

--- jest.config.js
+++ jest.config.js
@@ -1,6 +1,8 @@
 module.exports = {
   testEnvironment: 'node',
   roots: ['<rootDir>/packages'],
+  testPathIgnorePatterns: ['/node_modules/', '/dist/'],
+  modulePathIgnorePatterns: ['<rootDir>/packages/core/dist/', '<rootDir>/packages/engine/dist/'],
   moduleNameMapper: {
     '^@coast/core$': '<rootDir>/packages/core/src',
     '^@coast/engine$': '<rootDir>/packages/engine/src',
